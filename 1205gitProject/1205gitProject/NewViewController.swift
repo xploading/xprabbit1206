d39b4034d5b1510969e5f968d1a4a2992325a51a
qianfeng <qianfeng@rabbit.local>
2016-12-06 10:04:21 +0800

    新建新的视图控制器

---
+++ 1205gitProject/1205gitProject/NewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewViewController.swift
+//  1205gitProject
+//
+//  Created by qianfeng on 2016/12/6.
+//  Copyright © 2016年 耿晓鹏. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
