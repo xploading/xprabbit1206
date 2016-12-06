0e8001e62d3c833d3ad842c6edf5e54ae857b017
xploading <xpmail94@163,com>
2016-12-06 10:44:00 +0800

    客户端提交

--- 1205gitProject/1205gitProject/ViewController.swift
+++ 1205gitProject/1205gitProject/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("我的是swift")
+        print("桌面客户端的操作")
     }
 
     override func didReceiveMemoryWarning() {
